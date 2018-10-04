d52050d4e5abdc2d3f17eb17ccc0c5eaa610745c
scheff173 <42610294+scheff173@users.noreply.github.com>
2018-10-04 11:00:01 +0200

    fix in colorToAlpha()
    The computed alpha value could exceed the range [0, 1].

--- color.cc
+++ color.cc
@@ -5,11 +5,16 @@
 void colorToAlpha(
   uint width, uint height, uint32 img[], const Vec3f &pivot, float exp)
 {
+  // maximum distance is length(Vec3f(1, 1, 1) - Vec3f(0, 0, 0))
+  const float dMax = sqrt(3.0f);
+  // scale d to range of [0, 90] degree
+  const float normD = 0.5f * (float)Pi / dMax;
+  // process any pixel of image
   const uint32 *end = img + width * height;
   for (; img != end; ++img) {
     const Vec3f color = rgbaToColor(*img).xyz();
     const float d = length(color - pivot);
-    const float alpha = 1.0f - pow(cos(d * (float)Pi), exp);
+    const float alpha = 1.0f - pow(cos(d * normD), exp);
     (*img &= 0x00ffffff);
     (*img |= (uint32)(0xff000000 * alpha) & (uint32)0xff000000);
   }
